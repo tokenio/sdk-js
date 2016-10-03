d1265e9eeec967cd99aeadf0a89d93c33f718530
Mariano Sorgente <mariano.sorgente@token.io>
2016-10-03 13:58:20 -0700

    stgw

--- src/constants.js
+++ src/constants.js
@@ -7,8 +7,8 @@ const signatureScheme = 'Token-Ed25519-SHA512';
 // const uriHost = 'http://localhost:8000';
 // const uriHostBank = 'http://localhost:8100';
 
-const uriHost = 'http://dev.api.token.io';
-const uriHostBank = 'http://dev.api.token.io:81';
+const uriHost = 'http://stg.api.token.io';
+const uriHostBank = 'http://stg.api.token.io:81';
 
 // Scheme for payment tokens
 const paymentTokenScheme = 'Pay/1.0';
