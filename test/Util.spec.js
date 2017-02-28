c37921d4755c5f9de3b06a537cb059a35d41bbbd
Mariano Sorgente <mariano.sorgente@token.io>
2017-02-27 18:06:52 -0800

    First try at fixing IE

--- test/Util.spec.js
+++ test/Util.spec.js
@@ -11,4 +11,17 @@ describe('Util', () => {
         assert.equal(Util.countDecimals(23.253), 3);
         assert.equal(Util.countDecimals(23.2537), 4);
     });
+
+    it('should get the bytes from a word', () => {
+        const word = -864391073;
+        var byte1 = Util.getByte(word, 0);
+        var byte2 = Util.getByte(word, 1);
+        var byte3 = Util.getByte(word, 2);
+        var byte4 = Util.getByte(word, 3);
+        assert.equal(byte1, 95);
+        assert.equal(byte2, 112);
+        assert.equal(byte3, 122);
+        assert.equal(byte4, 204);
+    });
 });
+
