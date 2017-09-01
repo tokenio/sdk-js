55e584e75c8bb3588e6f9f0e72584eb8e9acb5fa
Larry Hosken <larry.hosken@token.io>
2017-09-01 12:15:53 -0700

    sample += how to set up a persistent keystore

---
+++ src/sample/PersistentlyCreateMemberSample.js
@@ -0,0 +1,29 @@
+/**
+ * Imports and sets up the SDK, and creates a Token member with the MemoryCryptoEngine
+ * (which will store keys in memory).
+ *
+ * @return {Member} member - created member
+ */
+export default async () => {
+    // Initialize SDK:
+    const TokenLib = require('../../src');
+    const Token = new TokenLib(
+        // sandbox test environment:
+        'sandbox',
+        // persist member secret keys in dir:
+        './keys');
+
+    // Generate a random-nonsense-string alias.
+    // ("name@token.io" would be more typical than a random string.
+    // But if we run this code with the same alias twice,
+    // the 2nd time it will fail because the name is taken.)
+    const alias = {
+        type: 'USERNAME',
+        value: Token.Util.generateNonce()
+    };
+
+    // Create a member with keys stored in memory:
+    return await Token.createMember(
+        alias,
+        Token.UnsecuredFileCryptoEngine);
+};
