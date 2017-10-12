6ceee6d3ae8c71667d8c0333ccb6f82ef5599caa
Sam Holeman <sam.holeman@token.io>
2017-10-12 16:48:20 -0700

    document waitUntil

--- test/TestUtil.js
+++ test/TestUtil.js
@@ -1,4 +1,12 @@
 class TestUtil {
+    /**
+     * Retries the supplied function until it either runs successfully or the timeout has passed.
+     *
+     * @param {function} fn - function to run until success or timeout
+     * @param {number} timeoutMs - milliseconds to wait before timing out
+     * @param {number} waitTimeMs - milliseconds to wait after failed invocation of the supplied
+     *                              function before retrying
+     */
     static async waitUntil(fn, timeoutMs = 10000, waitTimeMs = 500) {
         const start = Date.now();
         // eslint-disable-next-line no-constant-condition
