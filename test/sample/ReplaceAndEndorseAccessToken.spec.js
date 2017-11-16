f6925cc70430115c25d91d4aa1b9514debca221c
Larry Hosken <larry.hosken@token.io>
2017-11-16 13:24:45 -0800

    add some waitUntil between creating tokens and getAccessTokens

--- test/sample/ReplaceAndEndorseAccessToken.spec.js
+++ test/sample/ReplaceAndEndorseAccessToken.spec.js
@@ -8,7 +8,8 @@ import LinkMemberAndBankSample from '../../src/sample/LinkMemberAndBankSample';
 import CreateAndEndorseAccessTokenSample from '../../src/sample/CreateAndEndorseAccessTokenSample';
 import GetAccessTokensSample from '../../src/sample/GetAccessTokensSample';
 import ReplaceAndEndorseAccessTokenSample
-    from '../../src/sample/ReplaceAndEndorseAccessTokenSample';
+  from '../../src/sample/ReplaceAndEndorseAccessTokenSample';
+import TestUtil from '../TestUtil';
 
 describe('ReplaceAndEndorseAccessTokenSample test', () => {
     it('Should run the sample', async () => {
@@ -18,8 +19,10 @@ describe('ReplaceAndEndorseAccessTokenSample test', () => {
 
         const member2Alias = await member2.firstAlias();
         await CreateAndEndorseAccessTokenSample(member, member2Alias);
-        const foundToken = await GetAccessTokensSample(member, member2Alias);
-        const something = await ReplaceAndEndorseAccessTokenSample(member, foundToken);
-        assert.deepEqual(something.payload.to.alias, member2Alias);
+        TestUtil.waitUntil(async () => {
+            const foundToken = await GetAccessTokensSample(member, member2Alias);
+            const something = await ReplaceAndEndorseAccessTokenSample(member, foundToken);
+            assert.deepEqual(something.payload.to.alias, member2Alias);
+        });
     });
 });
