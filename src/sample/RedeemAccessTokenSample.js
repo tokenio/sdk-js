2438e20790470647c8e9db145442cb95486013f5
Larry Hosken <larry.hosken@token.io>
2018-01-18 13:16:54 -0800

    sample code += how to use an access token that maybe does not have allAccounts

--- src/sample/RedeemAccessTokenSample.js
+++ src/sample/RedeemAccessTokenSample.js
@@ -1,21 +1,75 @@
 /**
- * Redeems an information access token token, in order to acquire names of the grantor's
- * accounts.
- *
- * @param {Member} grantee - grantee member
- * @param {string} tokenId - id of the token to redeem
- * @return {Object} balance0 - balance of one account
+ * Sample code illustrating how a grantee might use an access token.
  */
-export default async (grantee, tokenId) => {
-    // Use the access token, now making API calls
-    // on behalf of the grantor, and get accounts
-    grantee.useAccessToken(tokenId);
-    const accounts = await grantee.getAccounts();
+class RedeemAccessTokenSample {
+    /**
+     * Redeems an information access token token to fetch an account balance.
+     * Assumes the token grants "allAccounts" access.
+     *
+     * @param {Member} grantee - grantee member
+     * @param {string} tokenId - id of the token to redeem
+     * @return {Object} balance0 - balance of one account
+     */
+    static async use(grantee, tokenId) {
+        // Use the access token, now making API calls
+        // on behalf of the grantor, and get accounts
+        grantee.useAccessToken(tokenId);
+        const accounts = await grantee.getAccounts();
 
-    // Get informtion we want:
-    const balance0 = await grantee.getBalance(accounts[0].id);
+        // Get informtion we want:
+        const balance0 = await grantee.getBalance(accounts[0].id);
 
-    // When done using access, clear the access token:
-    grantee.clearAccessToken();
-    return balance0.current;
-};
+        // When done using access, clear the access token:
+        grantee.clearAccessToken();
+        return balance0.current;
+    }
+
+    /**
+     * Redeems an information access token token to fetch an account balance.
+     * Does not assume the token grants "allAccounts" access.
+     *
+     * @param {Member} grantee - grantee member
+     * @param {string} tokenId - id of the token to redeem
+     * @return {Object} balance0 - balance of one account
+     */
+    static async carefullyUse(grantee, tokenId) {
+        var accessToken = await grantee.getToken(tokenId);
+        while (accessToken.replacedByTokenId) {
+            accessToken = await grantee.getToken(accessToken.replacedByTokenId);
+        }
+        var accountIds = {};
+        var haveAllBalancesAccess = false;
+        var haveAllAccountsAccess = false;
+        var i;
+        for (i = 0; i < accessToken.payload.access.resources.length; i++) {
+            const resource = accessToken.payload.access.resources[i];
+            if (resource.balance && resource.balance.accountId) {
+                accountIds[resource.balance.accountId] = true;
+                continue;
+            }
+            if (resource.allBalances) {
+                haveAllBalancesAccess = true;
+                continue;
+            }
+            if (resource.allAccounts) {
+                haveAllAccountsAccess = true;
+                continue;
+            }
+        }
+        grantee.useAccessToken(accessToken.id);
+        if (haveAllBalancesAccess && haveAllAccountsAccess) {
+            const accounts = await grantee.getAccounts();
+            for (i = 0; i < accounts.length; i++) {
+                accountIds[accounts[i].id] = true;
+            }
+        }
+        if (Object.keys(accountIds).length < 1) {
+            return {};
+        }
+        const account0Id = Object.keys(accountIds)[0];
+        const balance0 = await grantee.getBalance(account0Id);
+        grantee.clearAccessToken();
+        return balance0.current;
+    }
+}
+export default RedeemAccessTokenSample;
