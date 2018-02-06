c77006fe540001322cb78dbb2d18994411fd9fe0
VKCR <vivek.claver@gmail.com>
2018-02-06 08:54:21 -0800

    adding getBalances call

---
+++ test/sample/MemberGetBalancesSample.spec.js
@@ -0,0 +1,18 @@
+/* eslint-disable new-cap */
+const chai = require('chai');
+const assert = chai.assert;
+
+import 'babel-regenerator-runtime';
+import CreateMemberSample from '../../src/sample/CreateMemberSample';
+import LinkMemberAndBankSample from '../../src/sample/LinkMemberAndBankSample';
+import MemberGetBalancesSample from '../../src/sample/MemberGetBalancesSample';
+
+describe('MemberGetBalancesSample test', () => {
+    it('Should run the sample', async () => {
+        const member = await CreateMemberSample();
+	await LinkMemberAndBankSample(member);
+        const sums = await MemberGetBalancesSample(member);
+
+        assert.equal(sums.EUR, 200);
+    });
+});
