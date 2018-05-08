123c6212bdfc0aabf3ebb63c1713657778b43b1f
VKCR <vivek.claver@gmail.com>
2018-05-08 14:56:14 -0700

    add delete member api call (#104)

---
+++ test/sample/DeleteMemberSample.spec.js
@@ -0,0 +1,20 @@
+/* eslint-disable new-cap */
+const chai = require('chai');
+const assert = chai.assert;
+
+import 'babel-regenerator-runtime';
+import CreateMemberSample from '../../src/sample/CreateMemberSample';
+import DeleteMemberSample from '../../src/sample/DeleteMemberSample';
+
+describe('DeleteMemberSample test', () => {
+    it('Should run the sample', async () => {
+        const member = await CreateMemberSample();
+        await member.aliases();
+        await DeleteMemberSample(member);
+        try {
+            await member.aliases();
+        } catch (err) {
+            assert.include(err.message, "MEMBER_ID_NOT_FOUND");
+        }
+    });
+});
