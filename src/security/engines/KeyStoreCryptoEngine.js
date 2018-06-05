86e9f35be74215e458939d38552396a48f403e1f
Gianluca Pane <31416110+gianluca-token@users.noreply.github.com>
2018-06-05 10:23:20 -0700

    PL-164: add crypto method to generate temporary keys (#112)

--- src/security/engines/KeyStoreCryptoEngine.js
+++ src/security/engines/KeyStoreCryptoEngine.js
@@ -34,6 +34,22 @@ class KeyStoreCryptoEngine {
     }
 
     /**
+     * Generate a keypair with expiration date and store it.
+     *
+     * @param {string} level - privilege level "LOW", "STANDARD", "PRIVILEGED"
+     * @param {string} expirationMs - expiration date of the key in milliseconds
+     * @return {Object} key
+     */
+    async generateTemporaryKey(level, expirationMs) {
+        const keypair = Crypto.generateTemporaryKeys(level, expirationMs);
+        var stored = await this._keystore.put(this._memberId, keypair);
+        if (stored && stored.secretKey) {
+            delete stored.secretKey;
+        }
+        return stored;
+    }
+
+    /**
      * Create a signer. Assumes we previously generated the relevant key.
      *
      * @param {string} level - privilege level "LOW", "STANDARD", "PRIVILEGED"
