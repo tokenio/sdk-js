488ad23f25aa60daf596bf0274ef9f36f1ee89eb
Greco Rubio <greco.rubio@token.io>
2018-06-08 18:08:38 -0500

    Add CancelAccessToken blocking tests

--- src/sample/CancelAccessTokenSample.js
+++ src/sample/CancelAccessTokenSample.js
@@ -3,12 +3,13 @@
  *
  * @param {Member} grantor - grantor member
  * @param {string} tokenId - id of the token to cancel
+ * @param {bool} nonBlocking - flag to create a non-blocking request
  * @return {Object} result - result of the token operation
  */
-export default async (grantor, tokenId) => {
+export default async (grantor, tokenId, nonBlocking) => {
     // Grantor gets the token to see details
     const accessToken = await grantor.getToken(tokenId);
 
     // Grantor cancels the token
-    return await grantor.cancelToken(accessToken);
+    return await grantor.cancelToken(accessToken, nonBlocking);
 };
