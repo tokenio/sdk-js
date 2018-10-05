807a0d0791798948dc05d7629ddc90b76b1624f4
VKCR <vivek.claver@gmail.com>
2018-10-04 17:08:29 -0700

    nit

--- test/sample/NotificationsSample.spec.js
+++ test/sample/NotificationsSample.spec.js
@@ -20,15 +20,13 @@ describe('NotificationsSample test', () => {
 
         const payerAlias = await payer.firstAlias();
 
-        // TODO to test
         const token = await payer.createTransferToken(100.00, 'EUR')
             .setAccountId(accounts[0].id)
-            .setRedeemerAlias(payerAlias)
             .setToAlias(await payee.firstAlias())
             .addDestination({account: {token: {memberId: payee.memberId()}}})
             .execute();
         await payer.endorseToken(token);
-        await payer.redeemToken(token, 100.00, 'EUR', 'transfer notify sample');
+        await payee.redeemToken(token, 100.00, 'EUR', 'transfer notify sample');
 
         await PollNotificationsSample.get(payee);
     });
