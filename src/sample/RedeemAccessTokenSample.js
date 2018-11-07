520040364286480d309475194689aeda682d5859
Yuhan Niu <yuhan.niu@token.io>
2018-11-07 16:14:11 +0000

    update sample to use forAccessToken

--- src/sample/RedeemAccessTokenSample.js
+++ src/sample/RedeemAccessTokenSample.js
@@ -16,14 +16,11 @@ class RedeemAccessTokenSample {
         // forAccessToken snippet begin
         // Use the access token, now making API calls
         // on behalf of the grantor, and get accounts
-        grantee.useAccessToken(tokenId);
-        const accounts = await grantee.getAccounts();
+        const grantor = grantee.forAccessToken(tokenId);
+        const accounts = await grantor.getAccounts();
 
         // Get informtion we want:
         const balance0 = await grantee.getBalance(accounts[0].id, config.KeyLevel.LOW);
-
-        // When done using access, clear the access token:
-        grantee.clearAccessToken();
         // forAccessToken snippet end
 
         return balance0.balance.current;
