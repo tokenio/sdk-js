9413cf2b2e4ea8cd8df502bfd8896c2193e10256
Harry Jian <harry@token.io>
2019-04-04 18:42:13 -0700

    Remove old SDK

--- new-sdks/tpp/sample/DeleteMemberSample.js
+++
@@ -1,9 +0,0 @@
-/**
- * Deletes a member.
- *
- * @param {Member} member - member that will be deleted
- */
-
-export default async member => {
-    await member.deleteMember();
-};
--- test/sample/CreateMemberSample.spec.js
+++
@@ -1,7 +0,0 @@
-import CreateMemberSample from '../../src/sample/CreateMemberSample';
-
-describe('CreateMemberSample test', () => {
-    it('Should run the sample', async () => {
-        await CreateMemberSample();
-    });
-});
