e60959af0c9d0ddb4555771461157dae3d7c0b75
Daniel Lin <daniel.lin@token.io>
2019-08-30 11:42:34 -0700

    added resolveTransferDestinations method for representable

--- tpp/src/main/Representable.js
+++ tpp/src/main/Representable.js
@@ -1,11 +1,11 @@
 // @flow
-import {Account} from '@token-io/core';
+import Util, {Account} from '@token-io/core';
 import type Member from './Member';
 import type {
     Balance,
     KeyLevel,
     Transaction,
-    StandingOrder,
+    StandingOrder, TransferDestination,
 } from '@token-io/core';
 
 /**
@@ -150,5 +150,16 @@ export default class Representable {
         keyLevel: KeyLevel,
     ): Promise<{standingOrders: Array<StandingOrder>, offset: string}> {
         return this._member.getStandingOrders(accountId, offset, limit, keyLevel);
+    }
+
+    /**
+     * Resolves transfer destinations for the given account ID.
+     *
+     * @param accountId - ID of account to resolve destinations for
+     * @return resolved transfer destinations
+     */
+    resolveTransferDestinations(accountId: string): Promise<TransferDestination> {
+        const res = this._member.resolveTransferDestinations(accountId);
+        return res.transferDestinations;
     }
 }
