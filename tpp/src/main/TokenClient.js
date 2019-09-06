24d772c900944302866d0662507cdae57c7038ac
Daniel Lin <daniel.lin@token.io>
2019-09-06 15:19:55 -0700

    added enum for AccountResourceType

--- tpp/src/main/TokenClient.js
+++ tpp/src/main/TokenClient.js
@@ -11,6 +11,7 @@ import StandingOrderTokenRequestBuilder from './StandingOrderTokenRequestBuilder
 import type {
     Alias,
     ResourceType,
+    AccountResourceType,
     Signature,
     TokenRequest,
     KeyStoreCryptoEngine,
@@ -82,7 +83,7 @@ export class TokenClient extends Core {
      * @return The created TokenRequestBuilder
      */
     createAccessTokenRequest(
-        resources: Array
+        resources: Object
     ): AccessTokenRequestBuilder {
         return Util.callSync(this.createAccessTokenRequest, () => {
             const payload = {
