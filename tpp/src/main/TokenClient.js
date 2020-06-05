5a7b093215435a854e8ac6843866aab664eb94a6
javidang <javidan.guliyev@token.io>
2020-06-05 16:29:41 -0700

    fix param description

--- tpp/src/main/TokenClient.js
+++ tpp/src/main/TokenClient.js
@@ -73,7 +73,7 @@ export class TokenClient extends Core {
      *
      * @param  alias - alias for the member
      * @param  CryptoEngine - engine to use for key creation and storage
-     * @param  memberType - member type of the Member, default is BUSINESS
+     * @param  memberType - member type of the Member
      * @param  realmId - (optional) member id of the Member to which this new member will belong
      * @return Promise of created Member
      */
