24d2dc25e5afd5af64a77d76b6374896a9bfacc8
hitendra-ap <60222489+hitendra-ap@users.noreply.github.com>
2020-06-18 01:01:52 -0700

    Revert "APE-get banks (#320)"
    This reverts commit a5e0631935f797192a29ed26d30bd183e365ba82.

--- core/src/main/TokenClient.js
+++ core/src/main/TokenClient.js
@@ -148,7 +148,6 @@ export class TokenClient {
             provider?: string,
             destinationCountry?: string,
             bankFeatures?: Object,
-            memberId?: string,
         }
     ): Promise<{banks: Array<Bank>, paging: Paging}> {
         return Util.callAsync(this.getBanks, async () => {
