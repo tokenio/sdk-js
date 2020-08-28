f20b5d6c146c8d3cdcea59cfd945bf35a4b7f4ec
Anna Nikulova <anna.nikulova@token.io>
2020-08-27 22:11:53 -0700

    PLT-179: include pspSubjectName into generated eidas certificate

--- tpp/test/sample/VerifyEidasSample.spec.js
+++ tpp/test/sample/VerifyEidasSample.spec.js
@@ -8,8 +8,9 @@ describe('VerifyEidasSample test', () => {
     it('Should create and verify business member', async () => {
         // generate: TPP authNumber, a key pair and an eIDAS certificate for the generated key
         const tppAuthNumber = Util.generateNonce();
+        const pspSubjectName = Util.generateNonce();
         const keyPair = forge.pki.rsa.generateKeyPair(2048, 0x10001);
-        const certificate = await TestUtil.generateEidasCertificate(keyPair, tppAuthNumber);
+        const certificate = await TestUtil.generateEidasCertificate(keyPair, tppAuthNumber, pspSubjectName);
 
         // create and onboard a member
         const member = await VerifyEidasSample.verifyEidas(
@@ -18,5 +19,8 @@ describe('VerifyEidasSample test', () => {
         assert.equal(aliases.length, 1);
         assert.equal(aliases[0].value, tppAuthNumber);
         assert.equal(aliases[0].type, 'EIDAS');
+        //TODO(PLT-179): uncomment after member service change is deployed
+        //const profile = await member.getProfile(member.memberId());
+        //assert.equal(profile.displayNameFirst, pspSubjectName);
     });
 });
