c17d570da9cb0719477c3eecf20d17d4eaa9b69b
Vanika Sahu <47911428+vanika-ap@users.noreply.github.com>
2020-09-22 12:15:47 +0530

    APE-327 | Send Customer User Agent in CustomerTrackingMetadata (#347)

--- core/src/http/AuthHeader.js
+++ core/src/http/AuthHeader.js
@@ -123,6 +123,10 @@ class AuthHeader {
                 customerTrackingData += ',token-customer-ip-address='
                     + context.customerTrackingMetadata.ipAddress;
             }
+            if(context.customerTrackingMetadata.userAgent !== undefined){
+                customerTrackingData += ',token-customer-user-agent='
+                    + context.customerTrackingMetadata.userAgent;
+            }
         }
         return customerTrackingData;
     }
