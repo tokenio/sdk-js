e7b8c37b64b5f5d9b1b367b1ea16ba0cfb6abcd7
Jules Patry <julespatry@gmail.com>
2021-07-20 20:46:03 -0400

    SMP-3142 | indents additional headers class

--- core/src/http/AdditionalHeaders.js
+++ core/src/http/AdditionalHeaders.js
@@ -1,17 +1,17 @@
 /**
  * Class to add additional headers.
  */
- class AdditionalHeaders {
-  /**
-   * Adds additional headers to request config.
-   *
-   * @param {Object} config - config of the request
-   */
-  addAdditionalHeaders(config) {
-    if (config.additionalHeaders) {
-      config.headers = {...config.headers, ...config.additionalHeaders}
+class AdditionalHeaders {
+    /**
+     * Adds additional headers to request config.
+     *
+     * @param {Object} config - config of the request
+     */
+    addAdditionalHeaders(config) {
+        if (config.additionalHeaders) {
+           config.headers = {...config.headers, ...config.additionalHeaders}
+        }
     }
-  }
 }
 
 export default AdditionalHeaders;
