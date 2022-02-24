fa53f53592f35e42994c3b5009129ad9106f0548
Heena Odedra <68951813+heena-ap@users.noreply.github.com>
2022-02-24 19:21:57 +0530

    APE-2013 | Add destination and customerData to tokenRequest Payload (#447)

--- tpp/sample/StoreAndRetrieveTransferTokenRequestSample.js
+++ tpp/sample/StoreAndRetrieveTransferTokenRequestSample.js
@@ -11,6 +11,19 @@ const Token = new TokenClient({env: TEST_ENV, developerKey: devKey, keyDir: './k
  */
 export default async payee => {
     const tokenRequest = Token.createTransferTokenRequest(10.00, 'EUR')
+        .addTransferDestination({
+            faster_payments: {
+                sort_code: 123456,
+                account_number: 12345678,
+            },
+            customerData: {
+                legalNames: ['Southside'],
+                address: {
+                    houseNumber: '10',
+                    city: 'London',
+                    state: 'UK',
+                }},
+        })
         .setToMemberId(payee.memberId())
         .setFromAlias('EMAIL', 'payerEmail@gmail.com')
         .setDescription('Book Purchase')
